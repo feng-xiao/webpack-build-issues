8eaa72de53023633d8374785c0f13f08af205ca8
Feng Xiao <fengxiao@toromontcat.com>
2017-04-01 01:03:21 -0400

    make repo smaller

--- config/ts-loader.js
+++ config/ts-loader.js
@@ -4,7 +4,7 @@ module.exports = function (source, sourceMap) {
     // Not cacheable during unit tests;
     this.cacheable && this.cacheable();
 
-    source = `import { Params } from '@angular/router';\nlet a: Params = {};\nconsole.log(a);\n` + source;
+    // source = `import { Params } from '@angular/router';\nlet a: Params = {};\nconsole.log(a);\n` + source;
 
     // Support for tests
     if (this.callback) {
